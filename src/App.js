23ef67dd5ecf121015a7d495189b0849bf31cb66
WangHngLeee <1023836798@qq.com>
2020-05-31 14:59:44 -0400

    ready for deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Cards, Charts, CountryPicker} from './components';
 import styles from './App.module.css';
 import { fetchData } from "./api"
-
+import covidimg from './images/image.png';
 class App extends React.Component{
     state={
         data: {},
@@ -26,6 +26,7 @@ class App extends React.Component{
         const {data,country} = this.state;
         return (
             <div className={styles.container}>
+                <img className={styles.image} src={covidimg} alt="COVID-19"/>
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange = {this.handleCountryChange}/>
                 <Charts data={data} country = {country}/>
