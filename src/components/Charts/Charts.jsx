afac767da78ad632ee932387e52a72e819462d1b
WangHngLeee <1023836798@qq.com>
2020-05-31 13:41:44 -0400

    finish card and chart components

---
+++ src/components/Charts/Charts.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect, useDebugValue} from "react";
+import {fetchDailyDate} from "../../api";
+import {Line,Bar} from "react-chartjs-2";
+import styles from './Charts.module.css';
+
+const Charts = () =>{
+    const [dailyData,setDailyData] = useState({});
+    useEffect(()=>{
+        const fetchAPI = async () =>{
+            setDailyData(await fetchDailyDate());
+        }
+        fetchAPI();
+    });
+    const lineChart =(
+        dailyData.length
+        ?(
+        <Line
+            data={{
+                labels: dailyData.map(({date})=>date),
+                datasets:[{
+                    data:dailyData.map(({ confirmed })=>confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill:true,
+                },{
+                    data:dailyData.map(({ deaths })=>deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255,0,0,0.5)',
+                    fill:true,
+
+                }],
+            }}
+        />) : null
+    );
+
+    return(
+        <div className={styles.container}>
+            {lineChart}
+        </div>
+    )
+}
+export default Charts;
