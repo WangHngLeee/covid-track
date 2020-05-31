aced4d1daf6f1be0ae6a2eb5c76ccae99c50474d
WangHngLeee <1023836798@qq.com>
2020-05-31 15:05:54 -0400

    Updates

---
+++ precache-manifest.3cc97fe63beabbcc6b270dba3cc4aaee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f2270b007c41022763d222fb343dc8",
+    "url": "/covid-track/index.html"
+  },
+  {
+    "revision": "258ab6388c42c3f07073",
+    "url": "/covid-track/static/css/main.236ac55f.chunk.css"
+  },
+  {
+    "revision": "5dab7fb8e3244b661ed6",
+    "url": "/covid-track/static/js/2.3be7f26d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-track/static/js/2.3be7f26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258ab6388c42c3f07073",
+    "url": "/covid-track/static/js/main.29894ba3.chunk.js"
+  },
+  {
+    "revision": "0a146564a094189a38d0",
+    "url": "/covid-track/static/js/runtime-main.77a1afb4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-track/static/media/image.d7265326.png"
+  }
+]);
